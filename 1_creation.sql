eee37006e9a6b15955611650f74ceda0284d626c
Kévin-Seng Tek <kevinsengtek707@gmail.com>
2026-03-09 16:30:46 +0100

    Update SQL

--- 1_creation.sql
+++ 1_creation.sql
@@ -1,142 +1,103 @@
 CREATE DATABASE pokemon_db;
 USE pokemon_db;
 
-CREATE TABLE Attaque (
-    atk_id INT AUTO_INCREMENT,
-    atk_nom VARCHAR(100),
-    atk_puissance SMALLINT,
-    atk_cout_energie BYTE,
-    atk_description VARCHAR(300),
-    PRIMARY KEY (atk_id)
+CREATE TABLE Attaque(
+   atk_id INT,
+   atk_nom VARCHAR(100),
+   atk_puissance SMALLINT,
+   atk_cout_energie BYTE,
+   atk_description VARCHAR(300),
+   PRIMARY KEY(atk_id)
 );
 
-CREATE TABLE Paquet (
-    paquet_code_SKU VARCHAR(30),
-    paquet_type VARCHAR(30),
-    paquet_prix_reference DECIMAL(6,2),
-    paquet_statut LOGICAL,
-    PRIMARY KEY (paquet_code_SKU)
+CREATE TABLE Paquet(
+   paquet_code_SKU VARCHAR(30),
+   paquet_type VARCHAR(30),
+   paquet_prix_reference DECIMAL(6,2),
+   paquet_statut LOGICAL,
+   PRIMARY KEY(paquet_code_SKU)
 );
 
-CREATE TABLE Extension (
-    ext_id INT AUTO_INCREMENT,
-    ext_nom VARCHAR(100),
-    ext_date_sortie DATE,
-    ext_generation SMALLINT,
-    PRIMARY KEY (ext_id)
+CREATE TABLE Extension(
+   ext_id INT,
+   ext_nom VARCHAR(100),
+   ext_date_sortie DATE,
+   ext_generation SMALLINT,
+   PRIMARY KEY(ext_id)
 );
 
-CREATE TABLE Livraison (
-    livraison_numero_suivi VARCHAR(50),
-    livraison_date_expedition DATE,
-    livraison_date_reception_prevue DATE,
-    livraison_statut VARCHAR(20),
-    PRIMARY KEY (livraison_numero_suivi)
+CREATE TABLE Livraison(
+   livraison_numero_suivi VARCHAR(50),
+   livraison_date_expedition DATE,
+   livraison_date_reception_prevue DATE,
+   livraison_statut VARCHAR(20),
+   PRIMARY KEY(livraison_numero_suivi)
 );
 
-CREATE TABLE Illustrateur (
-    ill_ID INT AUTO_INCREMENT,
-    ill_nom VARCHAR(100),
-    ill_nationalite VARCHAR(50),
-    ill_statut LOGICAL,
-    PRIMARY KEY (ill_ID)
+CREATE TABLE Illustrateur(
+   ill_ID INT,
+   ill_nom VARCHAR(100),
+   ill_nationalite VARCHAR(50),
+   ill_statut LOGICAL,
+   PRIMARY KEY(ill_ID)
 );
 
-CREATE TABLE Lieu (
-    lieu_ID INT AUTO_INCREMENT,
-    lieu_nom VARCHAR(100),
-    lieu_type VARCHAR(30),
-    lieu_pays VARCHAR(50),
-    lieu_adresse VARCHAR(200),
-    lieu_stocks_paquets INT,
-    lieu_statut LOGICAL,
-    PRIMARY KEY (lieu_ID)
+CREATE TABLE Lieu(
+   lieu_ID INT,
+   lieu_nom VARCHAR(100),
+   lieu_type VARCHAR(30),
+   pays VARCHAR(50),
+   adresse VARCHAR(200),
+   stocks_paquets INT,
+   lieu_statut LOGICAL,
+   PRIMARY KEY(lieu_ID)
 );
 
-CREATE TABLE Carte (
-    c_id INT AUTO_INCREMENT,
-    c_pos_serie SMALLINT,
-    c_type VARCHAR(20),
-    c_nom VARCHAR(100),
-    c_langue VARCHAR(30),
-    c_rarete VARCHAR(30),
-    c_PV SMALLINT,
-    c_resistance VARCHAR(50),
-    c_faiblesse VARCHAR(50),
-    c_texte_effet VARCHAR(500),
-    c_date_sortie DATE,
-    c_statut LOGICAL,
-    c_id_provient_de INT,
-    paquet_code_SKU VARCHAR(30),
-    ext_id INT,
-    PRIMARY KEY (c_id),
-
-    FOREIGN KEY (c_id_provient_de)
-        REFERENCES Carte(c_id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (paquet_code_SKU)
-        REFERENCES Paquet(paquet_code_SKU)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (ext_id)
-        REFERENCES Extension(ext_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+CREATE TABLE Carte(
+   c_id INT,
+   c_pos_série SMALLINT,
+   c_type VARCHAR(20),
+   c_nom VARCHAR(100),
+   c_langue VARCHAR(30),
+   c_rarete VARCHAR(30),
+   c_PV SMALLINT,
+   c_résistance VARCHAR(50),
+   c_faiblesse VARCHAR(50),
+   c_texte_effet VARCHAR(500),
+   c_date_sortie DATE,
+   c_statut LOGICAL,
+   c_id_provient_de INT,
+   paquet_code_SKU VARCHAR(30),
+   ext_id INT NOT NULL,
+   PRIMARY KEY(c_id),
+   UNIQUE(c_id_provient_de),
+   FOREIGN KEY(c_id_provient_de) REFERENCES Carte(c_id),
+   FOREIGN KEY(paquet_code_SKU) REFERENCES Paquet(paquet_code_SKU),
+   FOREIGN KEY(ext_id) REFERENCES Extension(ext_id)
 );
 
-CREATE TABLE Possede (
-    c_id INT,
-    atk_id INT,
-    PRIMARY KEY (c_id, atk_id),
-
-    FOREIGN KEY (c_id)
-        REFERENCES Carte(c_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (atk_id)
-        REFERENCES Attaque(atk_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+CREATE TABLE Possède(
+   c_id INT,
+   atk_id INT,
+   PRIMARY KEY(c_id, atk_id),
+   FOREIGN KEY(c_id) REFERENCES Carte(c_id),
+   FOREIGN KEY(atk_id) REFERENCES Attaque(atk_id)
 );
 
-CREATE TABLE A_dessine (
-    c_id INT,
-    ill_ID INT,
-    PRIMARY KEY (c_id, ill_ID),
-
-    FOREIGN KEY (c_id)
-        REFERENCES Carte(c_id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (ill_ID)
-        REFERENCES Illustrateur(ill_ID)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+CREATE TABLE A_dessiné(
+   c_id INT,
+   ill_ID INT,
+   PRIMARY KEY(c_id, ill_ID),
+   FOREIGN KEY(c_id) REFERENCES Carte(c_id),
+   FOREIGN KEY(ill_ID) REFERENCES Illustrateur(ill_ID)
 );
 
-CREATE TABLE Distribue (
-    paquet_code_SKU VARCHAR(30),
-    livraison_numero_suivi VARCHAR(50),
-    lieu_ID INT,
-    PRIMARY KEY (paquet_code_SKU, livraison_numero_suivi, lieu_ID),
-
-    FOREIGN KEY (paquet_code_SKU)
-        REFERENCES Paquet(paquet_code_SKU)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (livraison_numero_suivi)
-        REFERENCES Livraison(livraison_numero_suivi)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (lieu_ID)
-        REFERENCES Lieu(lieu_ID)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+CREATE TABLE Distribue(
+   paquet_code_SKU VARCHAR(30),
+   livraison_numero_suivi VARCHAR(50),
+   lieu_ID INT,
+   PRIMARY KEY(paquet_code_SKU, livraison_numero_suivi, lieu_ID),
+   FOREIGN KEY(paquet_code_SKU) REFERENCES Paquet(paquet_code_SKU),
+   FOREIGN KEY(livraison_numero_suivi) REFERENCES Livraison(livraison_numero_suivi),
+   FOREIGN KEY(lieu_ID) REFERENCES Lieu(lieu_ID)
 );
